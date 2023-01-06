03d6d8f2bd7254023a60d5325d8def37cd3f120c
dmitry.tyutryumov <contr.dmitry.tyutryumov@pandadoc.com>
2023-01-06 13:58:01 +0100

    Added mock server + auth routes

---
+++ mocks/routes/auth.js
@@ -0,0 +1,53 @@
+const USER = { userName: 'Mock User', logo: null, email: 'mock@mock.email' }
+const AUTH_TOKEN = {
+  accessToken: 'Mock-Access-Token',
+  refreshToken: 'Mock-Refresh-Token',
+}
+
+module.exports = [
+  {
+    id: 'signup', // route id
+    url: '/api/signup', // url in express format
+    method: 'POST', // HTTP method
+    variants: [
+      {
+        id: 'success', // variant id
+        type: 'json', // variant handler id
+        options: {
+          status: 200, // status to send
+          body: AUTH_TOKEN, // body to send
+        },
+      },
+    ],
+  },
+  {
+    id: 'login', // route id
+    url: '/api/login', // url in express format
+    method: 'POST', // HTTP method
+    variants: [
+      {
+        id: 'success', // variant id
+        type: 'json', // variant handler id
+        options: {
+          status: 200, // status to send
+          body: AUTH_TOKEN, // body to send
+        },
+      },
+    ],
+  },
+  {
+    id: 'get-user', // route id
+    url: '/api/user/:id', // url in express format
+    method: 'GET', // HTTP method
+    variants: [
+      {
+        id: 'success', // variant id
+        type: 'json', // variant handler id
+        options: {
+          status: 200, // status to send
+          body: USER, // body to send
+        },
+      },
+    ],
+  },
+]
